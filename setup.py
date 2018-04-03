e41332529cbd4af88f15954474a1192f96895b12
ACCarnall <adamc@roe.ac.uk>
2018-04-03 13:37:12 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -26,12 +26,12 @@ setup(
     packages=["bagpipes"],
 
     package_data={
-    "tables/IGM" : ["Lyman_series_coefs_Inoue_2014_Table2.txt"]
-    "tables/nebular/bc03_miles" : ["*.fits"]
-    "tables/stellar/bc03_miles" : ["*.fits"]
-    "tables/dust/bc03_miles" : ["*.txt"]
+    "tables/IGM" : ["Lyman_series_coefs_Inoue_2014_Table2.txt"],
+    "tables/nebular/bc03_miles" : ["*.fits"],
+    "tables/stellar/bc03_miles" : ["*.fits"],
+    "tables/dust/bc03_miles" : ["*.txt"],
     "" : ["*.rst", "*.txt"]
-    }
+    },
 
     install_requires=['numpy', "corner", "pymultinest", "matplotlib", "scipy", "astropy"],  
 
