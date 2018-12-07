74285a823fdc15ef948d7c876f06bce20f30d280
ACCarnall <adamc@roe.ac.uk>
2018-12-07 17:23:16 +0000

    added george dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='bagpipes',
 
-    version='0.7.2',
+    version='0.7.3',
 
     description='Galaxy spectral fitting',
 
@@ -31,7 +31,7 @@ setup(
 
     install_requires=["numpy>=1.14.2", "corner", "pymultinest", "astropy",
                       "matplotlib>=2.2.2", "scipy", "msgpack", "deepdish",
-                      "pandas"],
+                      "pandas", "george"],
 
     project_urls={
         "readthedocs": "https://bagpipes.readthedocs.io",
