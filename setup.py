e2d73dc30bdc67452dd862efe753960faae0bdd1
Adam Carnall <adam@Adams-MBP.lan>
2020-06-01 16:56:34 +0100

    v0.8.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='bagpipes',
 
-    version='0.8.2',
+    version='0.8.3',
 
     description='Galaxy spectral fitting',
 
