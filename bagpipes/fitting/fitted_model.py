a08fdb624fdb3669a5d1a89a5b74cafe77353172
Pietro Benotto <81818996+pietro31700@users.noreply.github.com>
2024-02-06 08:15:14 +0100

    Fix indentation

--- bagpipes/fitting/fitted_model.py
+++ bagpipes/fitting/fitted_model.py
@@ -96,18 +96,18 @@ class fitted_model(object):
                             hyp_key = all_keys[i][len(prior_key)+1:]
                             self.hyper_params[-1][hyp_key] = all_vals[i]
 
-            # Find any parameters which mirror the value of a fit param.
-            if all_vals[i] in all_keys:
-                self.mirror_pars[all_keys[i]] = all_vals[i]
-
-            if all_vals[i] == "dirichlet":
-                n = all_vals[all_keys.index(all_keys[i][:-6])]
-                comp = all_keys[i].split(":")[0]
-                for j in range(1, n):
-                    self.params.append(comp + ":dirichletr" + str(j))
-                    self.pdfs.append("uniform")
-                    self.limits.append((0., 1.))
-                    self.hyper_params.append({})
+                # Find any parameters which mirror the value of a fit param.
+                if all_vals[i] in all_keys:
+                    self.mirror_pars[all_keys[i]] = all_vals[i]
+    
+                if all_vals[i] == "dirichlet":
+                    n = all_vals[all_keys.index(all_keys[i][:-6])]
+                    comp = all_keys[i].split(":")[0]
+                    for j in range(1, n):
+                        self.params.append(comp + ":dirichletr" + str(j))
+                        self.pdfs.append("uniform")
+                        self.limits.append((0., 1.))
+                        self.hyper_params.append({})
 
         # Find the dimensionality of the fit
         self.ndim = len(self.params)
