b8ccda424648f4f3810e372429a130a7b038fa56
Thomas Harvey PhD2022 <tharvey@morgan.jb.man.ac.uk>
2024-08-09 17:34:25 +0100

    bumping version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='bagpipes',
 
-    version='1.1.2',
+    version='1.2.0',
 
     description='Galaxy spectral fitting',
 
